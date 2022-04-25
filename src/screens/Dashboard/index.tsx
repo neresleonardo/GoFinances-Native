df9de757698461e6d897df019248de4491f7305e
neresleonardo <leonardo.borges.neres@gmail.com>
2022-04-25 12:40:04 -0300

    style global

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import {
+    Container,
+    Title
+       } from './styles'
+
+export function Dashboard() {
+    return (
+        <Container>
+            <Title>Ola</Title>
+        </Container>
+    )
+}
+
+  
